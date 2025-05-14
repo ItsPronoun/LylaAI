e92c2cfcc7c9cb5046ba0a1a44e4be708a8f101f
Shawn Johnson <github.unwarlike987@passmail.net>
2025-05-14 16:00:22 -0400

    Added some foundation files required for RO client to load our AI.

---
+++ AI_M.lua
@@ -0,0 +1,18 @@
+--[[
+
+    This file is a part of LylaAI, a custom homunculus/mercenary AI for Ragnarok Online.
+
+    All homunculus/mercenary AI begins with a file named AI.lua/AI_M.lua. This file is
+    loaded by the Ragnarok Online client at runtime, and several times per second the
+    function named `AI(id)` is called to perform AI functionality.
+    
+    This AI is free to distribute and to make changes. However, I encourage you to
+    submit bugs and feature requests to the official GitHub repository so we can make this 
+    a better AI together.
+
+]]
+
+--- This function is the entry point into the mercenary AI.
+--- @param id (number): Actor ID of the mercenary.
+function AI(id)
+end
